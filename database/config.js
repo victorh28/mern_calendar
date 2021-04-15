99a9112455e8910f018ce211c454f77f32aabe67
Victor Urra <vurra@andesst.cl>
2021-04-15 16:21:23 -0400

    db

--- database/config.js
+++ database/config.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() =>{
 
+    console.log(process.env.DB_CNN);
+
     try {
         await mongoose.connect(process.env.DB_CNN, {
              useNewUrlParser: true, 
